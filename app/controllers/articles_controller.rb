a1c062931b9da7075561b7853cbc8c05c48f9b39
shaurabhbhatt <shaurabhbhat@gmail.com>
2018-04-14 04:21:29 -0400

    Add user data to articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,8 @@ end
   
   def create
     @article = Article.new(article_params)
+    @article.user = User.first
+    debugger
     if @article.save
       flash[:success] = "Article  was successfully created"
       redirect_to article_path(@article)
