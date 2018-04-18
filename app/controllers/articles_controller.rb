83a45a451c39210871b1944bd78cf29ec9481fc8
shaurabhbhatt <shaurabhbhat@gmail.com>
2018-04-18 11:24:47 -0400

    User authenticaiton and user edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -9,11 +11,10 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-  
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
-end
+  end
   
   def show
     
@@ -49,6 +50,14 @@ end
   def set_article
     @article = Article.find(params[:id])
   end
+  
+  def require_same_user
+    if current_user != @article.user
+      flash[:error] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end
+  end
+  
   def article_params
     params.require(:article).permit(:title,:description)
   end
